48f5bcc3022cf7c2bd1bdc197a457fc84f77e7b6
Felix <felixgohh08@gmail.com>
2024-07-29 23:46:38 +0700

    add styles value

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: '10px',
+        sm: '12px',
+        base: '14px',
+        lg: '16px',
+        xl: '18px',
+        '2xl': '24px',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      boxShadow: {
+        popover: '0px 5px 15px 0px rgba(0, 0.25, 0.25, 0.25)',
+      },
+    },
   },
   plugins: [],
 };
